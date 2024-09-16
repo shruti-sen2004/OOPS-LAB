0ec3295fd7371df48f9bb1bc208dfeee8fc71deb
Shruti Sen <senshruti2020@gmail.com>
2024-09-16 07:27:51 +0530

    Add another method to solve Pig Latin

--- Miscellenious/piglatin.java
+++ Miscellenious/piglatin.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-class piglatin {
+//METHOD 1
+class piglatin_m1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the string: ");
@@ -18,11 +19,11 @@ class piglatin {
     }
 
     private static String convertToPigLatin(String word) {
-        int len = word.length();
         int i;
-        for (i = 0; i < len; i++) {
+        for (i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-            if (isVowel(ch)) {
+            ch = Character.toLowerCase(ch);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
                 break;
             }
         }
@@ -30,9 +31,34 @@ class piglatin {
         String first = word.substring(0, i);
         return rest + first + "ay";
     }
+}
 
-    private static boolean isVowel(char ch) {
-        ch = Character.toLowerCase(ch);
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+//METHOD 2
+class piglatin_m2{
+    public static String topiglatin(String word){
+        if (word == null || word.length() == 0) {
+            return word;
+        }
+        word = word.toLowerCase();
+        String vowels= "aeiou";
+        int firstVowelIndex = -1;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (vowels.indexOf(ch) != -1) {
+                firstVowelIndex = i;
+                break;
+            }
+        }
+        if (firstVowelIndex == -1) {
+            return word + "ay";
+        }
+        
+        String result = word.substring(firstVowelIndex) + word.substring(0, firstVowelIndex) + "ay";
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(topiglatin("king"));
+        System.out.println(topiglatin("hlleo"));
     }
 }
